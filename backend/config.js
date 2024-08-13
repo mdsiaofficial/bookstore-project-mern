add3a933677dce2d2cee2ea153d8da4444e3bafb
mdsiaofficial <mdsiaofficial@gmail.com>
2024-08-13 16:59:38 +0600

    aa

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;
-export const mongodbURL = "mongodb+srv://mdsiaofficial:bookstore1212@bookstore.z1zmicf.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstore";
+export const mongodb_url = `mongodb+srv://mdsiaofficial:bookstore@bookstore.gmxq3.mongodb.net/store-collection?retryWrites=true&w=majority&appName=bookstore`;
--- backend/index.js
+++ backend/index.js
@@ -1,56 +1,38 @@
 import express from "express";
-import { PORT, mongodbURL } from "./config.js";
-import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+import { PORT } from "./config";
 const app = express();
-app.use(express.json());
+
+
+// app.get("/", (req, res) => {
+//   console.log("header : - ", req.header);
+//   console.log("body : - ", req.body);
+//   console.log("query : - ", req.query);
+//   console.log("url : - ", req.url);
+//   console.log("baseUrl : - ", req.baseUrl);
+//   console.log("method : - ", req.method);
+//   console.log("cookies : - ", req.cookies);
+//   console.log("params : - ", req.params);
+//   // res.status(201).send(req);
+//   res.status(201).json({ message: "OK" });
+// });
+
+// app.post("/", (req, res) => {
+//   console.log("header : - ", req.header);
+//   console.log("body : - ", req.body);
+//   console.log("query : - ", req.query);
+//   console.log("url : - ", req.url);
+//   console.log("baseUrl : - ", req.baseUrl);
+//   console.log("method : - ", req.method);
+//   console.log("cookies : - ", req.cookies);
+//   console.log("params : - ", req.params);
+//   // res.status(201).send(req);
+//   res.status(201).cookie("auth", "fdasgsfdhrts54dfgdgt54").json({ message: "OK" });
+// });
+
 
 app.get("/", (req, res) => {
-  console.log(req);
-  return res.status(200).send("Welcome to bookstore.");
+  res.status(201).json({ message: "OK" });
 });
-
-//////////////////////////////
-// route for save a new book
-app.post("/books", async (req, res) => {
-
-  const { title, author, publishYear } = req.body;
-  console.log(title);
-  try {
-    if (!title && !author && !year) {
-      return (
-        res.status(400).send({ message: "All fields are required." })
-      );
-    };
-
-    const newBook = {
-      title,
-      author,
-      publishYear
-    };
-    console.log(newBook);
-    const book = await Book.create(newBook);
-    console.log(book);
-    
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({message:"Server Error"});
-  }
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server is running on 5555`);
 });
-
-
-////////////////////////////
-// mongoose connect here ///
-mongoose
-  .connect(mongodbURL)
-  .then(() => {
-    console.log("Connected to MongoDB.");
-    // 
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-
-  })
-  .catch((error) => {
-    console.log(error);
-  })
