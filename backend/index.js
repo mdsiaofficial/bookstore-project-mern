52bebac7e8d8675e77c51c20953eb53c85865ac7
mdsiaofficial <mdsiaofficial@gmail.com>
2024-08-13 17:57:27 +0600

    "Added MongoDB connection, book model, and API routes for creating, reading, and retrieving books"

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { PORT } from "./config";
-const app = express();
+import { PORT, mongodb_url } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/book.models.js";
 
+const app = express();
+app.use(express.json());
 
 // app.get("/", (req, res) => {
 //   console.log("header : - ", req.header);
@@ -29,10 +32,66 @@ const app = express();
 //   res.status(201).cookie("auth", "fdasgsfdhrts54dfgdgt54").json({ message: "OK" });
 // });
 
-
+// routes
 app.get("/", (req, res) => {
-  res.status(201).json({ message: "OK" });
+  return res.status(201).json({ message: "OK" });
 });
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`Server is running on 5555`);
+app.post("/books", async (req, res) => {
+  console.log(req.body);
+  const { title, author, publishYear } = req.body;
+
+  try {
+    if (!title && !author && !publishYear) {
+      return res.status(400).send({ message: "All fields are required." });
+    }
+
+    // create new
+    const newBook = { title, author, publishYear };
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+    
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+  
 });
+
+// get all books
+app.get("/books", async (req, res) => {
+  
+  try {
+    const books = await Book.find();
+    return res.status(200).json({ count: books.length, data: books });
+
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+});
+
+// get 
+app.get("/books/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(mongodb_url)
+  .then(() => {
+    console.log("Application connected to DB.");
+
+    // server connect
+    app.listen(process.env.PORT || PORT, () => {
+      console.log(`Server is running on 5555`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  })
